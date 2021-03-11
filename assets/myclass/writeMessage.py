7b8e8a463050356a0513492cea98667a19523ba5
Saverio <personnex976@gamil.com>
2021-03-11 20:24:32 +0100

    [ADD] methods to pad down and up and a signal (green text) message

--- assets/myclass/writeMessage.py
+++ assets/myclass/writeMessage.py
@@ -22,8 +22,12 @@ class WriteMessage:
         self._history = []
         curses.start_color()
         if curses.has_colors():
+            self._curses_color = True
             curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
             curses.init_pair(2, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+            curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
+        else:
+            self._curses_color = False
         self.re_init()
 
     def re_init(self):
@@ -31,8 +35,9 @@ class WriteMessage:
         self._pad.keypad(True)
         self._pad.refresh(0,0, self._uly,self._ulx, self._lry, self._lrx)
         self._counter = 0
+        self._y = 0
         for data in self._history:
-            data[0](*[data[x] for x in range(1, len(data))], False)
+            data[0](*[data[x] for x in range(1, len(data))], history=False)
             self.pad_refresh()
         
     def write_new_message(self, author, message, history=True):
@@ -51,7 +56,7 @@ class WriteMessage:
         list_message = self.split_large_text(f"{author} : {message}")
         for msg in list_message:
             self._pad.addstr(self._counter,0, msg)
-            self._counter += 1
+            self._counter += 1; self._y += 1
 
         self.pad_refresh()
         if history:
@@ -72,11 +77,11 @@ class WriteMessage:
 
         list_message = self.split_large_text(f"!!--> {author} : {message}")
         for msg in list_message:
-            if curses.has_colors():
+            if self._curses_color:
                 self._pad.addstr(self._counter,0, msg, curses.color_pair(2))
             else:
                 self._pad.addstr(self._counter,0, msg)
-            self._counter += 1
+            self._counter += 1; self._y += 1
 
         self.pad_refresh()
         if history:
@@ -96,15 +101,40 @@ class WriteMessage:
 
         list_message = self.split_large_text(data)
         for msg in list_message:
-            if curses.has_colors():
+            if self._curses_color:
                 self._pad.addstr(self._counter,0, msg, curses.color_pair(1))
             else:
                 self._pad.addstr(self._counter,0, msg)
-            self._counter += 1
+            self._counter += 1; self._y += 1
         
         self.pad_refresh()
         if history:
             self.add_to_history((self.write_system_message, data))
+
+    def write_signal_message(self, *args, history=True):
+        """
+        goal :
+            write signal message (with green color)
+        arg :
+            *args : all arg that you want
+        return :
+            None
+        """
+        if self._counter >= self._max_line*2/3:
+            self.re_init()
+
+        message = " ".join(args)
+        list_message = self.split_large_text(message)
+        for msg in list_message:
+            if self._curses_color:
+                self._pad.addstr(self._counter,0, msg, curses.color_pair(3))
+            else:
+                self._pad.addstr(self._counter,0, msg)
+            self._counter += 1; self._y += 1
+        
+        self.pad_refresh()
+        if history:
+            self.add_to_history((self.write_signal_message, *args))
 
     def write_start_up_message(self, pseudo):
         """
@@ -117,7 +147,7 @@ class WriteMessage:
         """
         self.write_system_message("Connection réalisée avec succès")
         self.write_system_message("Voici ton pseudo : ")
-        if curses.has_colors():
+        if self._curses_color:
             self._pad.addstr(1,19, pseudo, curses.color_pair(2))
         else:
             self._pad.addstr(1,19, pseudo)
@@ -126,19 +156,70 @@ class WriteMessage:
         
         self.pad_refresh()
 
+    def PadUP(self, nb):
+        """
+        goal :
+            go up in history message
+        arg :
+            nb : number to up
+        return :
+            None
+        """
+        self._y -= nb
+        if self._y < 0:
+            self._y = 0
+        self.pad_refresh()
+
+    def PadDOWN(self, nb):
+        """
+        goal :
+            go down in history message
+        arg :
+            nb : number to up
+        return :
+            None
+        """
+        self._y += nb
+        if self._y > self._counter:
+            self._y = self._counter
+        self.pad_refresh()
+
     def pad_refresh(self):
+        """
+        goal :
+            refresh pad screen to be in the right position self._y
+        arg : no arg
+        return :
+            None
+        """
         if self._counter <= self._lry - self._uly -1:
             self._pad.refresh(0,0, self._uly,self._ulx, self._lry, self._lrx)
         else:
-            pos_y = self._counter - self._lry - self._uly + 1
+            pos_y = self._y - self._lry - self._uly + 1
             self._pad.refresh(pos_y, 0, self._uly,self._ulx, self._lry, self._lrx)
 
     def add_to_history(self, data):
+        """
+        goal :
+            get a trace of the last 20 message
+        arg :
+            data : (pointer_to_the_write_type, *args)
+        return :
+            None
+        """
         if len(self._history) > 20:
             del self._history[0]
         self._history.append(data)
 
     def split_large_text(self, data):
+        """
+        goal :
+            split text to fit in the window
+        arg :
+            data : the whole data to write
+        return :
+            list of split message
+        """
         if len(data) >= curses.COLS: # pylint: disable=no-member
             l_data = []
             while len(data) >= curses.COLS: # pylint: disable=no-member
