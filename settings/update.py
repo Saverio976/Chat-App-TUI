27b8b1c93aa14065f80a441995406f75fade9d30
Saverio <personnex976@gamil.com>
2021-03-12 15:51:24 +0100

    [ADD] make possibility to download new update from github

---
+++ settings/update.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import requests
+from shutil import unpack_archive
+
+def update():
+    """
+    goal :
+        update all file
+    arg : no
+    return : no
+    """
+    repository_download = "https://api.github.com/repos/Saverio976/Chat-App-TUI/zipball/main"
+    name = "Chat-App-TUI-main.zip"
+    r = requests.get(repository_download).raw
+    with open(name, "wb") as fd:
+        for chunk in r.iter_content(chunk_size=128):
+            fd.write(chunk)
+    unpack_archive(name, "..", "zip")
+
+    print("Up To Date")
+    sys.exit()
