d8cbaae193d70a82d329b6798b4d693c31638537
Saverio <personnex976@gamil.com>
2021-03-14 22:45:27 +0100

    [UPDATE] now update clear

--- settings/update.py
+++ settings/update.py
@@ -3,7 +3,7 @@ file to update the directory (download from github)
 """
 import sys
 import requests
-from shutil import unpack_archive
+from shutil import unpack_archive, rmtree
 
 def update():
     """
@@ -13,12 +13,20 @@ def update():
     return : no
     """
     repository_download = "https://github.com/Saverio976/Chat-App-TUI/archive/main.zip"
-    name = "Chat-App-TUI-main.zip"
+    name = "../Chat-App-TUI-main.zip"
     r = requests.get(repository_download, stream=True)
     with open(name, "wb") as fd:
         for chunk in r.iter_content(chunk_size=128):
             fd.write(chunk)
+    try:
+        rmtree("../Chat-App-TUI-main", ignore_errors=True)
+    except:
+        pass
     unpack_archive(name, "..", "zip")
+    try:
+        rmtree(name, ignore_errors=True)
+    except:
+        pass
     print("Up To Date")
     sys.exit()
 
